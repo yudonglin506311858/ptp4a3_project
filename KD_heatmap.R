2f17aa0ed5f8fcbd43ef533006afc0630e84f7b1
yudonglin506311858 <506311858@qq.com>
2022-04-06 01:36:49 +0800

    Create KD_heatmap.R

---
+++ KD_heatmap.R
@@ -0,0 +1,173 @@
+Sys.setenv(LANGUAGE = "en") #显示英文报错信息
+gc()
+memory.limit(9999999999)
+set.seed(123)
+rm(list = ls())  
+options(stringsAsFactors = F)
+library(Seurat)
+library(tidyverse)
+library(patchwork)
+library(SCENIC)
+library(plyr)
+library(permute)
+library(data.table)
+library(SCopeLoomR)
+library(pheatmap)
+library("biomaRt")
+library("org.Hs.eg.db")
+library("org.Mm.eg.db")
+library("DESeq2")
+library(edgeR)
+library(limma)
+library("Rgraphviz")
+setwd("D:/anxiiuli_data/ptp4a3/count")
+
+setwd("D:/课题总结/ptp4a3转录组分析/figure3-kd转录组分析")
+
+
+
+fpkm<-read.csv(file="ternimalE_fpkm.csv",row.names = 1)
+head(fpkm)
+
+data <-fpkm
+head(data)
+
+genes<-read.csv("D:/anxiiuli_data/ptp4a3/count/KEGG_up_kk2.csv",row.names = 1)
+genes<-genes[c("mmu04151"),]
+genes<-genes[c("mmu04110"),]
+
+genes<-genes$geneID
+vec <- unlist(strsplit(genes,split='/'))
+
+#删掉所有列上都重复的
+newdata<-data[c(vec),]
+newdata<-na.omit(newdata)
+#低值为蓝色，高值为红色，中间值为白色：
+#pdf("fig1g.pdf")
+pheatmap(newdata,fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("white","red","darkred"))(100))
+
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("navyblue","white","red"))(100))
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("blue","white","red"))(100))
+
+pheatmap(newdata,fontsize = 7,
+         cluster_rows = T,border_color = NA,cluster_cols = T,color = colorRampPalette(colors = c("white","red","darkred"))(100))
+
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = T,border_color = NA,cluster_cols = T,color = colorRampPalette(colors = c("navyblue","white","red"))(100))
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("blue","white","red"))(100))
+
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = T,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("blue","white","red"))(100))
+
+
+
+
+# 加载包
+library(Seurat)
+library(dplyr)
+library(ggplot2)
+library(pheatmap)
+library(RColorBrewer)
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = T,border_color = NA,cluster_cols = T,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+
+pheatmap(newdata,scale = "row",fontsize = 7,clustering_method = "ward.D2",
+         cluster_rows = T,border_color = NA,cluster_cols = F,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+
+
+
+
+pheatmap(newdata,scale = "row",fontsize = 6,filename = "new2.pdf",width = 10,height = 100,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+dev.off()
+
+
+
+
+write.csv(newdata,"pi3k基因表达fpkm——ptp4a3.csv")
+write.csv(newdata,"细胞周期基因表达fpkm.csv")
+
+
+
+
+
+library(KEGGREST)
+gs<-keggGet('mmu04151')
+gs<-keggGet('mmu04110')
+
+
+names(gs[[1]]) # 说明书里发现的哈
+class(gs[[1]]$GENE)#先看下数据类型，再做相应的处理
+
+genes <- unlist(lapply(gs[[1]]$GENE,function(x) strsplit(x,';')[[1]][1]))
+genes
+
+
+genes<-read.table("D:/课题总结/ptp4a3转录组分析/figure3-kd转录组分析/细胞周期基因.txt")
+genes<-genes$V1
+genes<-c("Cul1","Mcm4","Cdkn1b","Cdkn2d","Plk1","Rb1","Rbl1","Stag2")
+#https://www.jianshu.com/p/e8b8896f64e9
+
+#删掉所有列上都重复的
+newdata<-data[c(genes),]
+newdata<-na.omit(newdata)
+#低值为蓝色，高值为红色，中间值为白色：
+#pdf("fig1g.pdf")
+pheatmap(newdata,fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("white","red","darkred"))(100))
+
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("navyblue","white","red"))(100))
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("blue","white","red"))(100))
+
+pheatmap(newdata,fontsize = 7,
+         cluster_rows = T,border_color = NA,cluster_cols = T,color = colorRampPalette(colors = c("white","red","darkred"))(100))
+
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = T,border_color = NA,cluster_cols = T,color = colorRampPalette(colors = c("navyblue","white","red"))(100))
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(colors = c("blue","white","red"))(100))
+
+
+
+
+# 加载包
+library(Seurat)
+library(dplyr)
+library(ggplot2)
+library(pheatmap)
+library(RColorBrewer)
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+pheatmap(newdata,scale = "row",fontsize = 7,
+         cluster_rows = T,border_color = NA,cluster_cols = T,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+
+pheatmap(newdata,scale = "row",fontsize = 7,clustering_method = "ward.D2",
+         cluster_rows = T,border_color = NA,cluster_cols = F,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+
+
+
+
+pheatmap(newdata,scale = "row",fontsize = 6,filename = "new2.pdf",width = 10,height = 100,
+         cluster_rows = F,border_color = NA,cluster_cols = F,color = colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(100))
+dev.off()
+
+
+
+
+write.csv(newdata,"所有pi3k基因表达fpkm——ptp4a3.csv")
+
+
+write.csv(newdata,"所有细胞周期基因表达fpkm——ptp4a3.csv")
+
+
+
+
+
